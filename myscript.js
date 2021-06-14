b63a517dedbc2ede4a2595019c892b78aaf910ee
Souvik Das <85279561+SouvikDas-git@users.noreply.github.com>
2021-06-14 11:25:34 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,23 +1,23 @@
 
 var btn = document.getElementById('btn');
-btn.onclick =function(){
+btn.onclick = function () {
   alert("hello");
 }
 
 $(document).ready(
-function(){
-  $('#btn').hover(
-    function(){
-      $("#h2").css("font-size",'40px');
-      $("#h2").text("Project X");
-      $("#h3").text("Beta Version");
-      $(".cont").css("color",'#fff');
-    },
-    function(){
-      $("#h2").css("font-size",'');
-      $("#h2").text("Hello Geeks!");
-      $("#h3").text("I am working in this Project.");
-      $(".cont").css("color",'#000');
-    }
-  )
-})
+  function () {
+    $('#btn').hover(
+      function () {
+        $("#h2").css("font-size", '40px');
+        $("#h2").text("Project X");
+        $("#h3").text("Beta Version");
+        $(".cont").css("color", '#fff');
+      },
+      function () {
+        $("#h2").css("font-size", '');
+        $("#h2").text("Hello Geeks!");
+        $("#h3").text("We are working in this Project.");
+        $(".cont").css("color", '#000');
+      }
+    )
+  })
