c8c09cec4dfc7e437723d7a8c55f1e648b110da0
nuraktar <nuraktar>
2021-06-14 20:08:30 +0530

    management spaces for better view

--- myscript.js
+++ myscript.js
@@ -1,25 +1,23 @@
-//new changes are made
 var int = 1; //counter var
 var str1 = document.getElementById('h2').innerHTML;
 var str2 = document.getElementById('h3').innerHTML;
 
 function myfun1(){
-    int=1;
-    $("#btn").css("height",'0');
-    $("#btn").css("width",'0');
-    $("#h2").css("font-size", '');
-    $("#h2").text(str1);
-    $("#h3").text(str2);
-    $(".cont").css("color", '#000');
+  int=1;
+  $("#btn").css("height",'0');
+  $("#btn").css("width",'0');
+  $("#h2").css("font-size", '');
+  $("#h2").text(str1);
+  $("#h3").text(str2);
+  $(".cont").css("color", '#000');
 }
 
 var btn = document.getElementById('btn');
-btn.onclick = function () {
-  if (int == 2){
-  myfun1();
+  btn.onclick = function () {
+    if (int == 2){
+    myfun1();
   }
 }
-
 
 $(document).ready(
   function () {
@@ -35,4 +33,5 @@ $(document).ready(
       },
       function(){myfun1();}
     )
-  })
+  }
+)
